ecbdf6d05021595dd7c84bef565c75e1ba564da1
DUONG Phu-Hiep <hduong@lemonway.com>
2021-08-11 01:42:16 +0200

    encapsulate bus class

--- src/NodeJsClient/src/index.ts
+++ src/NodeJsClient/src/index.ts
@@ -49,14 +49,6 @@ export class WrappedRequest<TRequest extends object> {
     message: TRequest
 }
 
-interface Bus {
-    connection: amqp.Connection;
-    channel: amqp.ConfirmChannel;
-    queueName: string;
-    exchange: amqp.Replies.AssertExchange;
-    queue: amqp.Replies.AssertQueue;
-}
-
 class ResponseConsumer<TResponse extends object> {
     futureResponse: Promise<WrappedResponse<TResponse>>;
     resolveResponse: (value: WrappedResponse<TResponse> | PromiseLike<WrappedResponse<TResponse>>) => void;
@@ -76,78 +68,88 @@ interface WaitingRequest {
     responseConsumer: ResponseConsumer<object>;
     
 }
-const WaitingRequestsCache: Record<string, WaitingRequest> = {};
 //const DeserializersCache: Record<string, deseri
 
-async function startBus(): Promise<Bus> {
-    const connection = await amqp.connect('amqp://localhost/?heartbeat=60');
-    connection.on('close', () => {
-        console.log("connection closed");
-        //TODO: scheduleReconnect(); rebuild the Bus
-    });
-    process.on('SIGINT', async () => {
-        await connection.close();
-    });
-    
-    const channel = await connection.createConfirmChannel();
-    channel.prefetch(1, true)
-    //await channel.prefetch(this.options.prefetchCount, this.options.globalPrefetch);
-    
-    const queueName = `bus-${nanoid()}`;
-    
-    const exchange = await channel.assertExchange(queueName, 'fanout', {
-        durable: false, 
-        autoDelete: true, 
-        arguments: {'x-expires': 60000},
-    });
-    const queue = await channel.assertQueue(queueName, {
-        durable: false, 
-        autoDelete: true, 
-        arguments: {'x-expires': 60000},
-    });
-    
-    await channel.bindQueue(queueName, queueName, '');
-    console.log('Queue:', queue.queue, 'MessageCount:', queue.messageCount, 'ConsumerCount:', queue.consumerCount);
+class Bus {
+    waitingRequestsCache: Record<string, WaitingRequest> = {};
 
-    const consumer = await channel.consume(queueName, msg => {
-        const wrappedResponseText = msg.content.toString();
-        const wrappedResponse = <WrappedResponse<object>>deserialize(WrappedResponse, wrappedResponseText);
-        const requestId = wrappedResponse?.requestId;
-        if (requestId) {
-            const waitingRequest = WaitingRequestsCache[requestId];
-            if (waitingRequest) {
-                waitingRequest.responseConsumer.resolveResponse(wrappedResponse);
-                clearTimeout(waitingRequest.ontimeout);
-                delete WaitingRequestsCache[requestId]
-                channel.ack(msg, false);
+    connectOptions: amqp.Options.Connect;
+    connection: amqp.Connection;
+    channel: amqp.ConfirmChannel;
+    queueName: string;
+    exchange: amqp.Replies.AssertExchange;
+    queue: amqp.Replies.AssertQueue;
+    consumer: amqp.Replies.Consume;
+    
+    constructor(connectOptions: amqp.Options.Connect) {
+        this.connectOptions = connectOptions;
+    }
+    
+    /**
+     * The bus will auto-deleted after 60s inactivity
+     * @param expiry 
+     */
+    public async start() {
+        this.waitingRequestsCache = {}; //clear the cache
+        this.connection = await amqp.connect(this.connectOptions);
+        this.connection.on('close', () => {
+            console.log("connection closed");
+            //TODO: scheduleReconnect(); rebuild the Bus
+        });
+        process.on('SIGINT', async () => {
+            await this.stop();
+        });
+        
+        this.channel = await this.connection.createConfirmChannel();
+        this.channel.prefetch(1, true)
+        //await channel.prefetch(this.options.prefetchCount, this.options.globalPrefetch);
+        
+        this.queueName = `bus-${nanoid()}`;
+        
+        this.exchange = await this.channel.assertExchange(this.queueName, 'fanout', {
+            durable: false, 
+            autoDelete: true, 
+            //arguments: {'x-expires': expiry},
+        });
+        this.queue = await this.channel.assertQueue(this.queueName, {
+            durable: false, 
+            autoDelete: true, 
+            //arguments: {'x-expires': expiry},
+        });
+        
+        await this.channel.bindQueue(this.queueName, this.queueName, '');
+        console.log('Queue:', this.queue.queue, 'MessageCount:', this.queue.messageCount, 'ConsumerCount:', this.queue.consumerCount);
+
+        this.consumer = await this.channel.consume(this.queueName, msg => {
+            const wrappedResponseText = msg.content.toString();
+            const wrappedResponse = <WrappedResponse<object>>deserialize(WrappedResponse, wrappedResponseText);
+            const requestId = wrappedResponse?.requestId;
+            if (requestId) {
+                const waitingRequest = this.waitingRequestsCache[requestId];
+                if (waitingRequest) {
+                    waitingRequest.responseConsumer.resolveResponse(wrappedResponse);
+                    clearTimeout(waitingRequest.ontimeout);
+                    delete this.waitingRequestsCache[requestId]
+                    this.channel.ack(msg, false);
+                }
+                else {
+                    console.log(`requestId ${requestId} not found`)
+                    this.channel.nack(msg, false, false);
+                }
             }
             else {
-                console.log(`requestId ${requestId} not found`)
-                channel.nack(msg, false, false);
+                console.log(`Missing requestId in the response ${wrappedResponseText}`);
+                this.channel.reject(msg, false);
             }
-        }
-        else {
-            console.log(`Missing requestId in the response ${wrappedResponseText}`);
-            channel.reject(msg, false);
-        }
-    });
-    console.log('consumerTag=', consumer.consumerTag);
-    
-    return {
-        connection,
-        channel,
-        queueName,
-        exchange,
-        queue
-    };
-}
-
-class RequestPublisher<TRequest extends object, TResponse extends object> {
-    bus: Bus;
-    constructor(bus: Bus) {
-        this.bus = bus
+        });
+        console.log('consumerTag=', this.consumer.consumerTag);
     }
-    public async getResponse(exchangeName: string, wrappedRequest: WrappedRequest<TRequest>): Promise<WrappedResponse<TResponse>> {
+    public async stop() {
+        console.log("stop bus.");
+        await this.connection.close();
+    }
+    
+    public async getResponse<TRequest extends object, TResponse extends object>(exchangeName: string, wrappedRequest: WrappedRequest<TRequest>): Promise<WrappedResponse<TResponse>> {
         const responseConsumer = new ResponseConsumer<TResponse>();
         wrappedRequest.requestId = uuidv4();
         
@@ -155,15 +157,15 @@ class RequestPublisher<TRequest extends object, TResponse extends object> {
             requestId: wrappedRequest.requestId,
             responseConsumer,
         }
-        waitingRequest.ontimeout = setTimeout(function(wr: WaitingRequest){
+        waitingRequest.ontimeout = setTimeout(function(args){
             //if this ever gets called we didn't get a response in a timely fashion
-            wr.responseConsumer.rejectResponse(new Error("timeout " + wr.requestId));
+            args.waitingRequest.responseConsumer.rejectResponse(new Error("timeout " + args.waitingRequest.requestId));
             //delete the entry from cache
-            delete WaitingRequestsCache[wr.requestId];
-          }, 1000, waitingRequest);
-        WaitingRequestsCache[wrappedRequest.requestId] = waitingRequest;
+            delete args.self.waitingRequestsCache[args.waitingRequest.requestId];
+          }, 1000, {waitingRequest, self: this});
+        this.waitingRequestsCache[wrappedRequest.requestId] = waitingRequest;
         
-        await this.bus.channel.publish(
+        await this.channel.publish(
             exchangeName,
             "",
             Buffer.from(serialize(wrappedRequest)),
@@ -172,27 +174,51 @@ class RequestPublisher<TRequest extends object, TResponse extends object> {
         
         return responseConsumer.futureResponse;
     }
+    
+    public getResponseUrl(): URL {
+        if (!this.queueName)
+            throw Error('Bus is not started');
+        const protocol = (this.connectOptions.protocol || 'amqp') + ':';
+        let url = new URL(`${protocol}//127.0.0.1`);
+        url.protocol = protocol;
+        url.host = this.connectOptions.hostname;
+        url.port = this.connectOptions.port ? this.connectOptions.port.toString(): null;
+        url.username = this.connectOptions.username || '';
+        url.password = this.connectOptions.password || '';
+        url.pathname = this.connectOptions.vhost || '';
+        url.pathname = url.pathname.endsWith('/') ? url.pathname + this.queueName : url.pathname + '/' + this.queueName;
+        url.searchParams.append("temporary", "true")
+        return url;
+    }
 }
+
 
 async function main() {
     try {
-        const bus = await startBus();
-        
-        const wrappedRequest: WrappedRequest<NewOrderCommand> = {
-            requestId: null,
-            responseAddress: `amqp://localhost/${bus.queueName}?temporary=true`,
-            messageType: ["urn:message:OrderService.API.Models:NewOrderCommand"],
-            message: {
-                statusCode: 2,
-                statusText: "foo",
-            }
-        };
-        const requestPublisher = new RequestPublisher<NewOrderCommand, Order>(bus);
-        const response = await requestPublisher.getResponse("OrderService.API.Models:NewOrderCommand", wrappedRequest);
-        
-        console.log('Got response', response);
-        
-        await bus.connection.close();
+        const bus = new Bus({
+            hostname: 'localhost',
+            vhost:'/',
+            protocol:'amqp',
+            heartbeat:60
+        });
+        await bus.start();
+        try {
+            const wrappedRequest: WrappedRequest<NewOrderCommand> = {
+                requestId: null,
+                responseAddress: bus.getResponseUrl().toString(),
+                messageType: ["urn:message:OrderService.API.Models:NewOrderCommand"],
+                message: {
+                    statusCode: 2,
+                    statusText: "foo",
+                }
+            };
+            console.log("ResponseAddress = ", wrappedRequest.responseAddress);
+            const response = await bus.getResponse<NewOrderCommand, Order>("OrderService.API.Models:NewOrderCommand", wrappedRequest);
+            console.log('Got response', response);
+        }
+        finally {
+            await bus.stop();
+        }
     }
     catch (e) {
         console.error("Failed", e.message)
